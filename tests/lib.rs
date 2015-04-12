f4703d95f7aa93cd322c7e7b8863e8abbb355abb
KeyboardFire <andy@keyboardfire.com>
2015-04-12 11:02:57 -0500

    add binary tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -10,3 +10,13 @@ fn test_encode() {
 fn test_decode() {
     assert_eq!(hamming(Hamming::Decode, "111011011001100110111100111110100000000000000000000000000000000".to_string()), "test".to_string());
 }
+
+#[test]
+fn test_encode_binary() {
+    assert_eq!(hamming(Hamming::EncodeBinary, "101010".to_string()), "111101001010000");
+}
+
+#[test]
+fn test_decode_binary() {
+    assert_eq!(hamming(Hamming::DecodeBinary, "111101001010000".to_string()), "101010");
+}
