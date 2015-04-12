ac4ae3b516e5da3bc62813f8102c7f13dae1acdb
KeyboardFire <andy@keyboardfire.com>
2015-04-12 11:13:33 -0500

    add error correction test

--- tests/lib.rs
+++ tests/lib.rs
@@ -20,3 +20,13 @@ fn test_encode_binary() {
 fn test_decode_binary() {
     assert_eq!(hamming(Hamming::DecodeBinary, "111101001010000".to_string()), "101010");
 }
+
+#[test]
+fn test_error_correction() {
+    let s = hamming(Hamming::Encode, "test".to_string()).into_bytes();
+    for i in 0..s.len() {
+        let mut s2 = s.clone();
+        s2[i] = if s2[i] == 48 { 49 } else { 48 };
+        assert_eq!(hamming(Hamming::Decode, String::from_utf8(s2).unwrap()), "test".to_string());
+    }
+}
